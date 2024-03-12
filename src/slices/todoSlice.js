023b0fd496dc2e31e925f373714178f09723162f
Kayo Vinicius <kayoribeirodev@gmail.com>
2024-03-12 11:49:35 -0300

    atualizando

--- src/slices/todoSlice.js
+++ src/slices/todoSlice.js
@@ -1,10 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { data } from "autoprefixer";
 
 
 export const todoSlice = createSlice({
   name: "todos",
   initialState: {
-    list:JSON.parse(localStorage.getItem("listItems")),
+    list:JSON.parse(localStorage.getItem("listItems"))?JSON.parse(localStorage.getItem("listItems")):[] ,
     filter: "all",
 
 
@@ -30,6 +31,7 @@ export const todoSlice = createSlice({
   },
     removeTodo: (state, action) => {
       state.list = state.list.filter((todo) => todo.id !== action.payload)
+      localStorage.removeItem(state.list.filter((todo) => todo.id !== action.payload))
     },
     filterTodo: (state, action) => {
       state.filter = action.payload
